96bff2e98b9c1f921a0c4afd0f5685bc516212fe
Victor Havran <victor.havran@yahoo.com>
2025-11-26 23:04:31 +0200

    Final touchup

--- src/main/java/com/example/demo/BooksSseController.java
+++ src/main/java/com/example/demo/BooksSseController.java
@@ -9,17 +9,27 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class BooksSseController {
 
     private final AllBooksSubject allBooksSubject;
+    private final BooksRepository booksRepository;
 
-    public BooksSseController(AllBooksSubject allBooksSubject) {
+    public BooksSseController(AllBooksSubject allBooksSubject, BooksRepository booksRepository) {
         this.allBooksSubject = allBooksSubject;
+        this.booksRepository = booksRepository;
     }
 
     @GetMapping(value = "/books-sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter getBooksSse() {
-        final SseEmitter emitter = new SseEmitter(0L);
+        final SseEmitter emitter = new SseEmitter(60000L);
         final SseObserver observer = new SseObserver(emitter);
         allBooksSubject.attach(observer);
 
+        // Send all existing books to the client
+        try {
+            for (Book book : booksRepository.findAll()) {
+                observer.update(book);
+            }
+        } catch (Exception e) {
+            allBooksSubject.detach(observer);
+        }
 
         emitter.onCompletion(() -> allBooksSubject.detach(observer));
         emitter.onTimeout(() -> allBooksSubject.detach(observer));
